33f8cef1701d7f5666d43b6c1733d1ef007d207d
Soin Roman <soinroma26@gmail.com>
2022-06-02 17:07:17 +0500

    - add adaptive for mobile

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ $(".nav-list").on("click", "a", function (event) {
     let top = $(id).offset().top - 100;
     $('body,html').animate({scrollTop: top}, 1000);
 });
+
+
+// Бургер Меню
+const menu = document.querySelector('.nav-menu');
+const menuButton = document.querySelector('.menu-button');
+const menuClose = document.querySelector('.menu-close');
+
+menuButton.addEventListener('click', ()=>{
+    menu.classList.add('is-active');
+    menuClose.classList.add('is-active');
+    document.body.style.overflow = 'hidden';
+});
+
+menuClose.addEventListener('click', ()=>{
+    menuClose.classList.remove('is-active');
+    menu.classList.remove('is-active');
+    document.body.style.overflow = 'auto';
+});
